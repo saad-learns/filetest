d2747ce20e62242d24700710ea00941449f04821
Sa'ad <masshrur.mia@gmail.com>
2022-09-17 18:34:50 +0100

    add assumption on existance of file

--- src/test/java/filetest/FileTest.java
+++ src/test/java/filetest/FileTest.java
@@ -1,27 +1,23 @@
 package filetest;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.io.TempDir;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.AccessMode;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-import static java.io.File.createTempFile;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 public class FileTest {
-
-    @TempDir
-    File m_tempDir;
 
     @Test
     void testFile() throws IOException {
         File file = new File("/tmp/saad.txt");
         Path path = file.toPath();
 
+        assumeTrue(file.exists());
         assertFalse(Files.isWritable(path));
     }
 
